7141777a33132fb6f78ac7deca7cdb04622526ea
edotgilmore <edotgilmore@gmail.com>
2019-11-29 16:36:13 -0600

    finished the cars project

--- days/07-09-data-structures/day_8_cars/cars.py
+++ days/07-09-data-structures/day_8_cars/cars.py
@@ -1,3 +1,7 @@
+import re
+import itertools
+
+
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
     'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
@@ -10,22 +14,47 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    output_str = ""
+    cnter = 0
+    models = cars['Jeep']
+
+    for model in models:
+        cnter += 1
+        if cnter >= len(models):
+            output_str += str(model)
+        else:
+            output_str += str(model) + ', '
+
+    # print(output_str)
+    return output_str
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    first_place_models = []
+    for key in cars.keys():
+        first_place_models.append(cars[key][0])
+    return first_place_models
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    models_nested = list(cars.values())
+    models_flat = list(itertools.chain(*models_nested))
+    r = re.compile((".*" + grep), re.IGNORECASE)
+    grep_models = list(filter(r.match, models_flat))
+    grep_models.sort()
+    return grep_models
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    temp_cars = []
+    for keys in list(cars.keys()):
+        temp_cars = cars[keys]
+        temp_cars.sort()
+        cars[keys] = temp_cars
+    return cars
